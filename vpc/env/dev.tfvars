c1c5cc68feb7ca43f5e7fef3add6c3e5a2177f91
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-23 07:02:13 +0000

    adding new file

--- vpc/env/dev.tfvars
+++ vpc/env/dev.tfvars
@@ -1,3 +1,3 @@
 env              = "dev"
 vpc_public_cidr  = "10.255.0.0/24"
-vpc_private_cidr = [""]
+vpc_private_cidr = ["10.0.0.0/22"]
--- vpc/main.tf
+++ vpc/main.tf
@@ -6,3 +6,8 @@ tags               = {
     
 }
 }
+
+resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.vpc_private_cidr
+}
