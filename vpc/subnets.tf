efa914477698e61348011cda2d0c90865534f996
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-23 08:04:48 +0000

    adding new file

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "private" {
   count      = length(var.subnet_private_cidr)
   vpc_id     = aws_vpc.vpc.id
   cidr_block = element(var.subnet_private_cidr, count.index)
-  availability_zone = element(var.AZS, count.index
+  availability_zone = element(var.AZS, count.index)
 
   tags = {
     Name = "private_subnet_${count.index +1}"
