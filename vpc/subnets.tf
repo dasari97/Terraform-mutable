c492c1f15850e7ea52429f04ec5a099820bea8bf
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-23 08:03:41 +0000

    adding new file

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "public" {
   count      = length(var.subnet_public_cidr)
   vpc_id     = aws_vpc.vpc.id
   cidr_block = element(var.subnet_public_cidr, count.index)
+  availability_zone = element(var.AZS, count.index)
 
   tags = {
     Name = "public_subnet_${count.index +1}"
@@ -12,6 +13,7 @@ resource "aws_subnet" "private" {
   count      = length(var.subnet_private_cidr)
   vpc_id     = aws_vpc.vpc.id
   cidr_block = element(var.subnet_private_cidr, count.index)
+  availability_zone = element(var.AZS, count.index
 
   tags = {
     Name = "private_subnet_${count.index +1}"
