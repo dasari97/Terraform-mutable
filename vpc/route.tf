c356a2bbc124549cc0f87fd16d8fef8968ae6c13
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-23 10:05:28 +0000

    adding new file

--- vpc/route.tf
+++ vpc/route.tf
@@ -41,3 +41,15 @@ resource "aws_route" "default_to_private" {
   destination_cidr_block    = var.vpc_private_cidr[0]
   vpc_peering_connection_id = aws_vpc_peering_connection.peer1.id
 }
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public.*.id)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public_routeTable.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private.*.id)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private_routeTable.id
+}
