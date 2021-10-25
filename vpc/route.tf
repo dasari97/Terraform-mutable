19f778d4541437487ad438e67d077515c7cd6639
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-25 13:14:55 +0000

    adding new file

--- vpc/route.tf
+++ vpc/route.tf
@@ -1,7 +1,13 @@
 resource "aws_route_table" "public_routeTable" {
   vpc_id = aws_vpc.vpc.id
 
-  route = []
+  route = [
+   {
+      cidr_block = var.default_vpc_cidr
+      vpc_peering_connection_id = aws_vpc_peering_connection.peer1.id
+    }
+    
+    ]
 
   tags = {
     Name = "Public_RT_${var.env}-vpc"
@@ -11,24 +17,29 @@ resource "aws_route_table" "public_routeTable" {
 resource "aws_route_table" "private_routeTable" {
   vpc_id = aws_vpc.vpc.id
 
-  route = []
+  route = [
+   {
+      cidr_block = "var.default_vpc_cidr"
+      vpc_peering_connection_id = aws_vpc_peering_connection.peer1.id
+    }
+  ]
 
   tags = {
     Name = "Private_RT_${var.env}-vpc"
   }
 }
 
-resource "aws_route" "Public-peer-add" {
-  route_table_id            = aws_route_table.public_routeTable.id
-  destination_cidr_block    = var.default_vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer1.id
-}
+#resource "aws_route" "Public-peer-add" {
+#  route_table_id            = aws_route_table.public_routeTable.id
+#  destination_cidr_block    = var.default_vpc_cidr
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer1.id
+#}
 
-resource "aws_route" "Private-peer-add" {
-  route_table_id            = aws_route_table.private_routeTable.id
-  destination_cidr_block    = var.default_vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer1.id
-}
+#resource "aws_route" "Private-peer-add" {
+#  route_table_id            = aws_route_table.private_routeTable.id
+#  destination_cidr_block    = var.default_vpc_cidr
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer1.id
+#}
 
 resource "aws_route_table_association" "public" {
   count          = length(aws_subnet.public.*.id)
