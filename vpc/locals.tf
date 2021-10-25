3ee082eee24048dc9181e16bb7ba289f0e2afac4
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-25 12:33:57 +0000

    adding new file

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -1,4 +1,4 @@
 locals {
-    public_cidr  = [tolist(var.vpc_public_cidr, ""])
+    public_cidr  = tolist([var.vpc_public_cidr, ""])
     all_vpc_cidr = concat(var.vpc_privarte_cidr, local.public_cidr)
 }
