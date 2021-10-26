0be07d7455ad8d2affe71e6f5973194499cbf0e9
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-26 10:26:16 +0000

    adding new file

--- databases/data.tf
+++ databases/data.tf
@@ -3,3 +3,13 @@ data "aws_ami" "ami" {
   name_regex       = "^Centos-7*"
   owners           = ["973714476881"]
   }
+  
+data "terraform_remote_state" "vpc" {
+  backend = "s3" 
+
+  config = {
+    bucket = "dasari1998"
+    key    = "Terraform-mutable/vpc/${var.env}/${var.env)_state_file_backup"
+    region = "us-east-1"
+  }
+}
