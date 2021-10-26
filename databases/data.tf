76bc9e75f00d4e51cc382f9216a2551fb76d9846
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-26 11:45:09 +0000

    adding new file

--- databases/data.tf
+++ databases/data.tf
@@ -13,3 +13,7 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+data "aws_secretsmanager_secret" "Dev_secret" {
+  arn = "arn:aws:secretsmanager:us-east-1:795902710157:secret:dev-fDI5DH"
+}
--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -66,3 +66,16 @@ resource "aws_security_group" "mongodb" {
     Name = "mongodb-${var.env}"
   }
 }
+
+resource "null_resource" "mongodb" {
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = data.Dev_secret.ssh_user
+      password = data.Dev_secret.ssh_pass
+      host     = aws_spot_instance_request.mongodb.private_ip
+    }
+  }
+  
+}
+
