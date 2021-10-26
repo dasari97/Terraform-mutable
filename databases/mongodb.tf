fae4966b92697bbbda46efc20cbd29e3175dd979
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-26 12:08:36 +0000

    adding new file

--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -67,5 +67,21 @@ resource "aws_security_group" "mongodb" {
   }
 }
 
-
+resource "null_resource" "mongodb" {
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = jsondecode(data.aws_secretsmanager_secret_version.Dev_secret.secret_string)["ssh_user"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.Dev_secret.secret_string)["ssh_pass"]
+      host     = aws_spot_instance_request.mongodb.private_ip
+    }
+    
+    inline = [
+      "yum install python3-pip -y",
+      "pip3 install pip --upgrade",
+      "pip3 install ansible==4.1.0"
+    ]
+  }
+  
+}
 
