821bcb4e47976c9cdac20f10e2f42d35da9444e6
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-26 09:27:51 +0000

    adding new file

---
+++ mongodb/data.tf
@@ -0,0 +1,6 @@
+data "aws_ami" "ami" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "^centos7"
+  owners           = ["973714476881"]
+  
--- vpc/data.tf
+++ vpc/data.tf
@@ -1,5 +1,1 @@
-data "aws_ami" "ami" {
-  executable_users = ["self"]
-  most_recent      = true
-  name_regex       = "^centos7"
-  owners           = ["973714476881"]
+data "aws_caller_identity" "account_ID" {}
