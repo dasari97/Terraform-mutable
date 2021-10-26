72b714d0458f2fda06659e11aa83a990b6e7ada8
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-26 12:51:58 +0000

    adding new file

---
+++ databases/elastic_cache.tf
@@ -0,0 +1,15 @@
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "redis-${var.env}"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x"
+  engine_version       = "6.x"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.subnet-group
+}
+
+resource "aws_elasticache_subnet_group" "subnet-group" {
+  name       = "redis-${var.env}"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+}
