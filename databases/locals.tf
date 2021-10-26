bd736b224185ef2a168e8719aee18226b71e276d
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-26 11:14:03 +0000

    adding new file

--- databases/locals.tf
+++ databases/locals.tf
@@ -1,3 +1,4 @@
 locals {
-    vpc_cidr = [data.terraform_remote_state.vpc.outputs.vpc_cidr, data.terraform_remote_state.vpc.outputs.default_vpc_cidr]
+    default_vpc_cidr  = tolist([data.terraform_remote_state.vpc.outputs.default_vpc_cidr, ""])
+    all_vpc_cidr = compact(concat(data.terraform_remote_state.vpc.outputs.vpc_cidr, local.default_vpc_cidr))
 }
