a7c467c3e0c70a877c15f78102e47f500fc531d0
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-26 12:18:28 +0000

    adding new file

--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -67,5 +67,22 @@ resource "aws_security_group" "mongodb" {
   }
 }
 
-
+resource "null_resource" "mongodb" {
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = jsondecode(data.aws_secretsmanager_secret_version.Dev_secret.secret_string)["ssh_user"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.Dev_secret.secret_string)["ssh_pass"]
+      host     = aws_spot_instance_request.mongodb.private_ip
+    }
+    
+    inline = [
+      "sudo yum install python3-pip -y",
+      "sudo pip3 install pip --upgrade",
+      "sudo pip3 install ansible==4.1.0",
+      "ansible-pull -i localhost -U https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/ansible-roboshop.git roboshop-pull.yml -e COMPONENT=mongodb"
+    ]
+  }
+  
+}
 
