9961edddf168f0a546eab91699229478618ae16f
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-27 06:40:10 +0000

    adding new file

---
+++ databases/mysql_rds.tf
@@ -0,0 +1,61 @@
+resource "aws_db_subnet_group" "mysql_subnets" {
+  name       = "${var.env}-db_sb-mysql"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids[1]
+
+  tags = {
+    Name = "${var.env}-db_sb-mysql"
+  }
+}
+
+resource "aws_security_group" "mysql" {
+  name        = "mysql-${var.env}"
+  description = "Allow mysql"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID 
+
+  ingress = [
+    {
+      description      = "mysql"
+      from_port        = 3306
+      to_port          = 3306
+      protocol         = "CNAME"
+      cidr_blocks      = local.all_vpc_cidr
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
+      security_groups  = []
+    }
+  ]
+
+  egress = [
+    {
+      description      = "ALL"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
+      security_groups  = []
+    }
+  ]
+  
+  tags = {
+    Name = "mysql-${var.env}"
+  }
+}
+
+resource "aws_db_instance" "mysql-${var.env}" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "terraform"
+  username             = jsondecode(data.aws_secretsmanager_secret_version.Dev_secret.secret_string)["mysql_id"]
+  password             = jsondecode(data.aws_secretsmanager_secret_version.Dev_secret.secret_string)["mysql_pass"]
+  parameter_group_name = "mysql5.7-${var.env}"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.mysql_subnets.name
+  vpc_security_group_ids = []aws_security_group.mysql.id]
+  identifier             = "mysql-${var.env}"
+}
