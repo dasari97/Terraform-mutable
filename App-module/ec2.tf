a6e0c28cf7e2daeee7ca14018e3e41029c6f7d89
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-28 15:17:22 +0000

    adding new file

--- App-module/ec2.tf
+++ App-module/ec2.tf
@@ -26,7 +26,8 @@ resource "aws_spot_instance_request" "spot_ins" {
 }
 
 resource "aws_ec2_tag" "spot_ins" {
-  resource_id = aws_spot_instance_request.spot_ins.spot_instance_id
+  count       = var.spot_ins
+  resource_id = element(aws_spot_instance_request.spot_ins.spot_instance_id, count.index)
   key         = "Name"
   value       = "${var.component}-${var.env}"
 }
