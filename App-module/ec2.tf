6e73932bba60f05fe5c2195f49124b157ca44961
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-28 15:11:47 +0000

    adding new file

--- App-module/ec2.tf
+++ App-module/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "od_ins" {
     count = var.od_ins
-    ami   = data.aws_ami.ami,id
+    ami   = data.aws_ami.ami.id
     instance_type = var,instance_type
     subnet_id     = element(data.terraform_remote_state.vpc.outputs.private_subnet_ids, count.index)
     vpc_security_group_ids = [aws_security_group.app.id]
