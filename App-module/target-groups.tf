337ee91ef923f577e7d893cdb039c55c8d68dcdd
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-30 06:21:25 +0000

    adding new file

--- App-module/target-groups.tf
+++ App-module/target-groups.tf
@@ -1,6 +1,15 @@
-resource "aws_lb_target_group" "test" {
+resource "aws_lb_target_group" "target-group" {
   name     = "${var.component}-${var.env}-TG"
   port     = var.port
   protocol = "HTTP"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+  health_check  = {
+    enabled = true
+    healthy_threshold = 2
+    interval = 5
+    timeout = 3
+    path  = "/health"
+    port  = var.port
+    unhealthy_threshold = 3
+  }
 }
