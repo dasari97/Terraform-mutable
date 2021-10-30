738bf273c0f558de571ea56902d8e95394952921
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-30 06:00:30 +0000

    adding new file

--- App-module/target-groups.tf
+++ App-module/target-groups.tf
@@ -1,5 +1,5 @@
 resource "aws_lb_target_group" "test" {
-  name     = "${var.component}-${var.env}_TG"
+  name     = "${var.component}-${var.env}-TG"
   port     = var.port
   protocol = "HTTP"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
