174870facee679f6cefc974b7680f886a3dd0d25
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-03 11:32:40 +0000

    adding new file

--- ALB/ALB_SG.tf
+++ ALB/ALB_SG.tf
@@ -52,6 +52,17 @@ resource "aws_security_group" "ALB_public_SG" {
       prefix_list_ids  = []
       self             = false
       security_groups  = []
+    },
+    {
+      description      = "https"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
+      security_groups  = []
     }
   ]
 
